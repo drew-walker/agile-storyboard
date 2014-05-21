b4c066a280d30760165348354ad621d27b452c61
Drew Walker <drew.walker@gmail.com>
2014-05-21 21:12:32 +1000

    Ignoring compiled templates.js for jshint task

--- Gruntfile.js
+++ Gruntfile.js
@@ -111,7 +111,8 @@ module.exports = function (grunt) {
             },
             all: [
                 'Gruntfile.js',
-                '<%= yeoman.app %>/scripts/{,*/}*.js'
+                '<%= yeoman.app %>/scripts/{,*/}*.js',
+                '!<%= yeoman.app %>/scripts/views/templates.js'
             ],
             test: {
                 options: {
